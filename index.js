5c1b4004f4cc78630f35f743e8f9653796cc39d3
MrSohailAhmad <ahmadsohail1819@gmail.com>
2024-02-25 01:01:02 +0500

    api_first_commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/home", (req, res) => {
+  res.send("This is Home");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
